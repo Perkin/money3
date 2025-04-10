b9102d078c47672a1ac0ce6931f25d4f636fcd6b
Dmitriy Soskov <dmitrii.soskov@borzodelivery.com>
2025-04-10 18:53:15 +0300

    Удаляем moment.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
             output: {
                 manualChunks: {
                     react: ['react', 'react-dom'],
-                    vendor: ['lodash', 'moment'],
+                    vendor: ['lodash'],
                 },
             },
         },
